50b829598503e9e7862d31d9cccefa216a3c6ca6
SasaV-Kolosek <85159536+SasaV-Kolosek@users.noreply.github.com>
2024-10-31 22:14:13 +0100

    Feature/fix sizing (#16)
    * footer updated
    
    * index page updated
    
    * pricing page btns update
    
    * updated pricing page
    
    * fixed js errors and added for pricing page
    
    * additional fixes
    
    * fix footer for mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,72 +8,72 @@ import "./controllers/circle_ci";
 import "./controllers/github_ci";
 
 $(document).on('turbo:load', function() {
-  // handle lg nav dropdown item
-  const productButton = document.getElementById('productButton');
-  const flyoutMenu = document.getElementById('flyoutMenu');
-  let isMenuVisible = false;
+  // // handle lg nav dropdown item
+  // const productButton = document.getElementById('productButton');
+  // const flyoutMenu = document.getElementById('flyoutMenu');
+  // let isMenuVisible = false;
 
-  productButton.addEventListener('click', () => {
-    isMenuVisible = !isMenuVisible;
-    if (isMenuVisible) {
-      flyoutMenu.classList.remove('hidden', 'opacity-0', 'translate-y-1');
-      flyoutMenu.classList.add('opacity-100', 'translate-y-0');
-    } else {
-      flyoutMenu.classList.remove('opacity-100', 'translate-y-0');
-      flyoutMenu.classList.add('opacity-0', 'translate-y-1');
-      setTimeout(() => {
-        flyoutMenu.classList.add('hidden');
-      }, 150); // Match the duration of the leaving transition
-    }
-  });
+  // productButton.addEventListener('click', () => {
+  //   isMenuVisible = !isMenuVisible;
+  //   if (isMenuVisible) {
+  //     flyoutMenu.classList.remove('hidden', 'opacity-0', 'translate-y-1');
+  //     flyoutMenu.classList.add('opacity-100', 'translate-y-0');
+  //   } else {
+  //     flyoutMenu.classList.remove('opacity-100', 'translate-y-0');
+  //     flyoutMenu.classList.add('opacity-0', 'translate-y-1');
+  //     setTimeout(() => {
+  //       flyoutMenu.classList.add('hidden');
+  //     }, 150); // Match the duration of the leaving transition
+  //   }
+  // });
 
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!productButton.contains(event.target) && !flyoutMenu.contains(event.target)) {
-        isMenuVisible = false;
-        flyoutMenu.classList.remove('opacity-100', 'translate-y-0');
-        flyoutMenu.classList.add('opacity-0', 'translate-y-1');
-        setTimeout(() => {
-          flyoutMenu.classList.add('hidden');
-        }, 150); // Match the duration of the leaving transition
-    }
-    if (!mobileMenuButton.contains(event.target) && !mobileMenu.contains(event.target)) {
-      isMobileMenuVisible = false;
-      mobileMenu.classList.add('hidden');
-    }
-  }
+  // // Close the dropdown if the user clicks outside of it
+  // window.onclick = function(event) {
+  //   if (!productButton.contains(event.target) && !flyoutMenu.contains(event.target)) {
+  //       isMenuVisible = false;
+  //       flyoutMenu.classList.remove('opacity-100', 'translate-y-0');
+  //       flyoutMenu.classList.add('opacity-0', 'translate-y-1');
+  //       setTimeout(() => {
+  //         flyoutMenu.classList.add('hidden');
+  //       }, 150); // Match the duration of the leaving transition
+  //   }
+  //   if (!mobileMenuButton.contains(event.target) && !mobileMenu.contains(event.target)) {
+  //     isMobileMenuVisible = false;
+  //     mobileMenu.classList.add('hidden');
+  //   }
+  // }
 
-  // handle mobile show/hide menu
-  const mobileMenuButton = document.getElementById('mobileMenuButton');
-  const mobileMenu = document.getElementById('mobileMenu');
-  const closeMobileMenu = document.getElementById('closeMobileMenu');
-  let isMobileMenuVisible = false;
+  // // handle mobile show/hide menu
+  // const mobileMenuButton = document.getElementById('mobileMenuButton');
+  // const mobileMenu = document.getElementById('mobileMenu');
+  // const closeMobileMenu = document.getElementById('closeMobileMenu');
+  // let isMobileMenuVisible = false;
 
-  mobileMenuButton.addEventListener('click', () => {
-      isMobileMenuVisible = !isMobileMenuVisible;
-      mobileMenu.classList.toggle('hidden');
-  });
-  closeMobileMenu.addEventListener('click', () => {
-    isMobileMenuVisible = false;
-    mobileMenu.classList.add('hidden');
-  });
+  // mobileMenuButton.addEventListener('click', () => {
+  //     isMobileMenuVisible = !isMobileMenuVisible;
+  //     mobileMenu.classList.toggle('hidden');
+  // });
+  // closeMobileMenu.addEventListener('click', () => {
+  //   isMobileMenuVisible = false;
+  //   mobileMenu.classList.add('hidden');
+  // });
 
-  // handle submenu mobile
-  const productSubMenuButton = document.getElementById('productSubMenuButton');
-  const productSubMenuIcon = document.getElementById('productSubMenuIcon');
-  const disclosure1 = document.getElementById('disclosure-1');
-  let isSubMenuVisible = false;
+  // // handle submenu mobile
+  // const productSubMenuButton = document.getElementById('productSubMenuButton');
+  // const productSubMenuIcon = document.getElementById('productSubMenuIcon');
+  // const disclosure1 = document.getElementById('disclosure-1');
+  // let isSubMenuVisible = false;
 
-  productSubMenuButton.addEventListener('click', () => {
-      isSubMenuVisible = !isSubMenuVisible;
-      if (isSubMenuVisible) {
-        disclosure1.classList.remove('hidden');
-        productSubMenuIcon.classList.add('rotate-180');
-      } else {
-        disclosure1.classList.add('hidden');
-        productSubMenuIcon.classList.remove('rotate-180');
-      }
-  });
+  // productSubMenuButton.addEventListener('click', () => {
+  //     isSubMenuVisible = !isSubMenuVisible;
+  //     if (isSubMenuVisible) {
+  //       disclosure1.classList.remove('hidden');
+  //       productSubMenuIcon.classList.add('rotate-180');
+  //     } else {
+  //       disclosure1.classList.add('hidden');
+  //       productSubMenuIcon.classList.remove('rotate-180');
+  //     }
+  // });
 
   // handle login dropdown
   // const dropdownButton = document.getElementById('loginButton');
@@ -90,13 +90,13 @@ $(document).on('turbo:load', function() {
   //   }
   // });
 
-  document.addEventListener('click', function(event) {
-    const isClickInside = dropdownButton.contains(event.target) || dropdownMenu.contains(event.target);
+  // document.addEventListener('click', function(event) {
+  //   const isClickInside = dropdownButton.contains(event.target) || dropdownMenu.contains(event.target);
   
-    if (!isClickInside && isDropdownMenuVisible) {
-      // Close the dropdown menu if the click was outside
-      dropdownMenu.classList.add('hidden');
-      isDropdownMenuVisible = false;
-    }
-  });
+  //   if (!isClickInside && isDropdownMenuVisible) {
+  //     // Close the dropdown menu if the click was outside
+  //     dropdownMenu.classList.add('hidden');
+  //     isDropdownMenuVisible = false;
+  //   }
+  // });
 });
