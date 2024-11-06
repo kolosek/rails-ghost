74c50a62ecccdaa40ff4db01f10190163943bcc7
SasaV-Kolosek <sasavas@kolosek.com>
2024-11-06 20:59:14 +0100

    add custom 404 error page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,9 +3,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 
   content: [
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*',
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
