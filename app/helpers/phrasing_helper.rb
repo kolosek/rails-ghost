5bebc4e008994add1b7d6970eae9652857810d92
SasaV-Kolosek <85159536+SasaV-Kolosek@users.noreply.github.com>
2025-10-06 12:03:09 +0200

    Phrasing content (#44)

--- app/helpers/phrasing_helper.rb
+++ app/helpers/phrasing_helper.rb
@@ -2,4 +2,10 @@ module PhrasingHelper
   def can_edit_phrases?
     current_user.present? && current_user.admin?
   end
+
+  def safe_phrase(key, default_content = "")
+    content = phrase(key) # call phrasing gem
+    text_only = ActionView::Base.full_sanitizer.sanitize(content.to_s)
+    text_only == key ? phrase(default_content) : content
+  end
 end
